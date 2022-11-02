00a8f7863e2897ba6a342684aad41f1432d077ca
Agnieszka Krzanowska <agnieszka.krzanowska1998@gmail.com>
2022-11-02 19:05:01 +0100

    arrow functions added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-console.log("Hello everyone! :D");
+{
+    const welcome = () => {
+        console.log("Hello everyone! :D");
+    };
 
-let button = document.querySelector(".js-button");
-let image = document.querySelector(".js-image");
-let activityName = document.querySelector(".js-button--activityName");
+    welcome();
 
-button.addEventListener("click", () => {
-    image.classList.toggle("hide");
-    activityName.innerText = image.classList.contains("hide") ? "pokaż" : "usuń";
+    const hideImage = () => {
+        const image = document.querySelector(".js-image");
+        const activityName = document.querySelector(".js-button--activityName");
 
-})
+        image.classList.toggle("hide");
+        activityName.innerText = image.classList.contains("hide") ? "pokaż" : "usuń";
+    };
+
+    const init = () => {
+        const button = document.querySelector(".js-button");
+        button.addEventListener("click", hideImage);
+    };
+
+    init();
+
+}
